899d831ea6af71053fae569f4a24ad099801cd5d
sakshi-200 <87275662+sakshi-200@users.noreply.github.com>
2021-07-13 15:39:40 +0530

    Update 11Updaterecord.py

--- 11Updaterecord.py
+++ 11Updaterecord.py
@@ -2,18 +2,14 @@ import mysql.connector as mycon
 
 con=mycon.connect(host='localhost',user='root',password='APPLE',database='bookstoredb')
 curs=con.cursor()
-
-bkcd=int(input('Enter bookcode : '))
-curs.execute("select * from books where bookcode=%d" %bkcd)
-rec=curs.fetchone()
-
-rw=input("give your review : ")
-curs.execute("Update books SET review='%s' "%(rw))
-con.commit()
-
-try:
-   print('Review updated sucessfully...')
-except :
-   print('book not found')
+try: 
+    bcd=int(input("Enter bookcode :"))
+    rw=input('Give your review : ')  
+    curs.execute("Update books set review='%s'where bookcode=%d" %(rw,bcd))
+    print("Update bookreview : ",rw)
+    con.commit()
+    
+except:
+    print('Book Not Found...')
 
 con.close()
