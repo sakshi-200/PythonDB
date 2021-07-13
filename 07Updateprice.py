9875b4140fccee3a515d2313b0fb35964018fa25
sakshi-200 <87275662+sakshi-200@users.noreply.github.com>
2021-07-13 15:36:20 +0530

    Update 07Updateprice.py

--- 07Updateprice.py
+++ 07Updateprice.py
@@ -2,17 +2,14 @@ import mysql.connector as mycon
 
 con=mycon.connect(host='localhost',user='root',password='APPLE',database='bookstoredb')
 curs=con.cursor()
+try: 
+    bcd=int(input("Enter bookcode :"))
+    prc=int(input('Enter price : '))    
+    curs.execute("Update books set price=%d where bookcode=%d" %(prc,bcd))
+    print("Update book price : ",prc)
+    curs.execute("select bookcode,booknm,category,author,publication,edition,year,price from books where bookcode=%d" %bcd)
+    rec=curs.fetchone()
 
-
-bcd=int(input('Enter bookcode : '))
-curs.execute("select bookcode,booknm,category,author,publication,edition,year,price from books where bookcode=%d" %bcd)
-rec=curs.fetchone()
-
-nwprc=int(input("Enter price : "))
-curs.execute=("Update books set price=%d" %(nwprc))
-con.commit()
-
-try:
     print("bookcode : %d" %rec[0])
     print("bookname : %s" %rec[1])
     print("Category : %s" %rec[2])
@@ -22,7 +19,7 @@ try:
     print("Year : %d" %rec[6])
     print("Price : %d" %rec[7]) 
     print('Book details found sucessfully...')
-except :
-    print('book not found')
-
-con.close()
+    con.commit()
+    
+except:
+    print('not found...')
